78ac3f5a827d8027e87f0a1693d51f638dd3bc7c
tudou <125363148+cxsfutur@users.noreply.github.com>
2023-11-25 11:44:53 +0800

    Add files via upload

---
+++ url-short/0/main.js
@@ -0,0 +1,63 @@
+let res
+  function shorturl() {
+    if(document.querySelector("#text").value==""){
+        alert("Url cannot be empty!")
+        return
+    }
+
+    document.getElementById("searchbtn").disabled=true;
+	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'请稍候……;
+    fetch(window.location.pathname, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: document.querySelector("#text").value })
+    }).then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    res = myJson;
+    document.getElementById("searchbtn").disabled=false;
+	document.getElementById("searchbtn").innerHTML='再来一次';
+    if(res.key!=="")
+    document.getElementById("result").innerHTML=window.location.host+res.key;
+    $('#exampleModal').modal('show')
+  }).catch(function(err){alert("未知错误，请重试！");
+  console.log(err);
+  document.getElementById("searchbtn").disabled=false;
+	document.getElementById("searchbtn").innerHTML=' 再来一次';})
+  }
+  function copyurl (id, attr) {
+    let target = null;
+
+    if (attr) {
+        target = document.createElement('div');
+        target.id = 'tempTarget';
+        target.style.opacity = '0';
+        if (id) {
+            let curNode = document.querySelector('#' + id);
+            target.innerText = curNode[attr];
+        } else {
+            target.innerText = attr;
+        }
+        document.body.appendChild(target);
+    } else {
+        target = document.querySelector('#' + id);
+    }
+
+    try {
+        let range = document.createRange();
+        range.selectNode(target);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+        console.log('Copy success')
+    } catch (e) {
+        console.log('Copy error')
+    }
+
+    if (attr) {
+        // remove temp target
+        target.parentElement.removeChild(target);
+    }
+  }
